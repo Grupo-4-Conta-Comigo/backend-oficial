f12ca97b36de3e1ce891e9c62ff09adbb7207937
R-e-i-s <rafael.creis@gmail.com>
2023-04-21 19:08:26 -0300

    adicionado swagger em endpoint de restaurante

--- src/main/java/comigo/conta/backend/oficial/api/controller/restaurante/RestauranteController.java
+++ src/main/java/comigo/conta/backend/oficial/api/controller/restaurante/RestauranteController.java
@@ -4,6 +4,10 @@ import comigo.conta.backend.oficial.service.autenticacao.dto.RestauranteLoginDto
 import comigo.conta.backend.oficial.service.autenticacao.dto.RestauranteTokenDto;
 import comigo.conta.backend.oficial.service.restaurante.RestauranteService;
 import comigo.conta.backend.oficial.service.restaurante.dto.RestauranteCriacaoDto;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -22,12 +26,30 @@ public class RestauranteController {
         this.restauranteService = restauranteService;
     }
 
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    content = @Content(schema = @Schema(hidden = true))
+            )
+    })
     @PostMapping("/criar")
     public ResponseEntity<Void> criar(@RequestBody @Validated RestauranteCriacaoDto restauranteCriacaoDto) {
         this.restauranteService.criar(restauranteCriacaoDto);
         return ResponseEntity.status(201).build();
     }
 
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    content = @Content(schema = @Schema(hidden = true))
+            )
+    })
     @PostMapping("/login")
     public ResponseEntity<RestauranteTokenDto> login(@RequestBody @Validated RestauranteLoginDto restauranteLoginDto) {
         RestauranteTokenDto restauranteTokenDto = this.restauranteService.autenticar(restauranteLoginDto);

2e1aae1d69b1670ea6e69e7ae46dcf6842d1b6f0
R-e-i-s <rafael.creis@gmail.com>
2023-04-21 19:29:08 -0300

    adicionado swagger em endpoint de comanda

--- src/main/java/comigo/conta/backend/oficial/api/controller/pedido/ComandaController.java
+++ src/main/java/comigo/conta/backend/oficial/api/controller/pedido/ComandaController.java
@@ -5,6 +5,10 @@ import comigo.conta.backend.oficial.service.pedido.PedidoService;
 import comigo.conta.backend.oficial.service.pedido.submodules.comanda.ComandaService;
 import comigo.conta.backend.oficial.service.pedido.submodules.comanda.dto.ComandaCriacaoDto;
 import comigo.conta.backend.oficial.service.pedido.submodules.comanda.dto.ComandaUpdateDto;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -23,6 +27,27 @@ public class ComandaController {
         this.comandaService = comandaService;
     }
 
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    content = @Content(schema = @Schema(hidden = true))
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    content = @Content(schema = @Schema(hidden = true))
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    content = @Content(schema = @Schema(hidden = true))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    content = @Content(schema = @Schema(hidden = true))
+            ),
+    })
     @PostMapping("/{idPedido}")
     public ResponseEntity<Comanda> postComanda(
             @RequestBody @Validated ComandaCriacaoDto comandaCriacaoDto,
@@ -31,6 +56,27 @@ public class ComandaController {
         return ResponseEntity.status(201).body(this.comandaService.criar(comandaCriacaoDto, idPedido));
     }
 
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200"
+            ),
+            @ApiResponse(
+                    responseCode = "204",
+                    content = @Content(schema = @Schema(hidden = true))
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    content = @Content(schema = @Schema(hidden = true))
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    content = @Content(schema = @Schema(hidden = true))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    content = @Content(schema = @Schema(hidden = true))
+            ),
+    })
     @GetMapping("/{idPedido}")
     public ResponseEntity<List<Comanda>> getAll(
             @PathVariable String idPedido,
@@ -40,11 +86,49 @@ public class ComandaController {
         return comandas.isEmpty() ? ResponseEntity.status(204).build() : ResponseEntity.ok(comandas);
     }
 
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200"
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    content = @Content(schema = @Schema(hidden = true))
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    content = @Content(schema = @Schema(hidden = true))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    content = @Content(schema = @Schema(hidden = true))
+            ),
+    })
     @GetMapping("/preco/{idComanda}")
     public ResponseEntity<Double> getPreco(@PathVariable String idComanda) {
         return ResponseEntity.ok(comandaService.getPreco(idComanda));
     }
 
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    content = @Content(schema = @Schema(hidden = true))
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    content = @Content(schema = @Schema(hidden = true))
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    content = @Content(schema = @Schema(hidden = true))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    content = @Content(schema = @Schema(hidden = true))
+            ),
+    })
     @PutMapping("/editar/{idComanda}")
     public ResponseEntity<Comanda> putComanda(
             @PathVariable String idComanda,
@@ -53,11 +137,45 @@ public class ComandaController {
         return ResponseEntity.ok(this.comandaService.editar(idComanda, comandaUpdateDto));
     }
 
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200"
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    content = @Content(schema = @Schema(hidden = true))
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    content = @Content(schema = @Schema(hidden = true))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    content = @Content(schema = @Schema(hidden = true))
+            ),
+    })
     @PatchMapping("/finalizar/{idComanda}")
     public ResponseEntity<Comanda> finalizarComanda(@PathVariable String idComanda) {
         return ResponseEntity.ok(this.comandaService.finalizar(idComanda));
     }
 
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200"
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    content = @Content(schema = @Schema(hidden = true))
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    content = @Content(schema = @Schema(hidden = true))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    content = @Content(schema = @Schema(hidden = true))
+            ),
+    })
     @DeleteMapping("/{idComanda}")
     public ResponseEntity<Void> deletarComanda(@PathVariable String idComanda) {
         this.comandaService.deletar(idComanda);

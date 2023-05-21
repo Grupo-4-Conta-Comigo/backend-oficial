df85295808f5847d2193772c09116b9f8bca5d94
cardosoOReis <rafaelcdsreis@gmail.com>
2023-05-20 22:01:29 -0300

    bugfix path da controller do calculo

--- src/main/java/comigo/conta/backend/oficial/api/controller/calculo/CalculoController.java
+++ src/main/java/comigo/conta/backend/oficial/api/controller/calculo/CalculoController.java
@@ -11,11 +11,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController("/calulos")
+@RestController
+@RequestMapping("/calculos")
 public class CalculoController {
     private final CalculoService calculoService;
 

52335fc95d4e024320772a0b643389bd3b712be7
cardosoOReis <rafaelcdsreis@gmail.com>
2023-11-17 21:33:20 -0300

    swagger

--- src/main/java/comigo/conta/backend/oficial/api/controller/mobile/ClienteController.java
+++ src/main/java/comigo/conta/backend/oficial/api/controller/mobile/ClienteController.java
@@ -38,6 +38,27 @@ public class ClienteController {
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    content = @Content(schema = @Schema(hidden = true))
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    content = @Content(schema = @Schema(hidden = true))
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    content = @Content(schema = @Schema(hidden = true))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    content = @Content(schema = @Schema(hidden = true))
+            ),
+    })
     @CrossOrigin
     @PostMapping("/login")
     public ResponseEntity<ClienteTokenDto> login(@RequestBody @Validated ClienteLoginDto clienteLoginDto) {
--- src/main/java/comigo/conta/backend/oficial/api/controller/mobile/PedidosMobileController.java
+++ src/main/java/comigo/conta/backend/oficial/api/controller/mobile/PedidosMobileController.java
@@ -2,9 +2,13 @@ package comigo.conta.backend.oficial.api.controller.mobile;
 
 import comigo.conta.backend.oficial.service.mobile.dtos.PedidoGenerico;
 import comigo.conta.backend.oficial.service.usuario.IntegracaoMobileService;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,10 +16,32 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/mobile/pedido")
+@Tag(name = "Pedidos Externos")
 @AllArgsConstructor
 public class PedidosMobileController {
     private final IntegracaoMobileService integracaoMobileService;
 
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    content = @Content(schema = @Schema(hidden = true))
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    content = @Content(schema = @Schema(hidden = true))
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    content = @Content(schema = @Schema(hidden = true))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    content = @Content(schema = @Schema(hidden = true))
+            ),
+    })
     @GetMapping
     public ResponseEntity<PedidoGenerico> getPedidos(
             @RequestParam String restauranteId,
